959a343d4d44b811378fe95ca2cab0783dc530f5
José Bulhões <josebgojunior@gmail.com>
2024-02-29 21:38:28 -0300

    quinto commit

--- Galeria-imagem/galeria.js
+++ Galeria-imagem/galeria.js
@@ -3,7 +3,35 @@ const botao2 = document.querySelector("#foto2")
 const botao3 = document.querySelector("#foto3")
 const botao4 = document.querySelector("#foto4")
 const botao5 = document.querySelector("#foto5")
+const imagemnova = document.querySelector("#imagemfull")
 
 botao1.addEventListener("click", (event) => {
+
+    imagemnova.src = botao1.src
+    imagemnova.style.display = "block"
+    
+});
+
+botao2.addEventListener("click", (event) => {
+
+    imagemnova.src = botao2.src
+    
+});
+
+botao3.addEventListener("click", (event) => {
+
+    imagemnova.src = botao3.src
+    
+});
+
+botao4.addEventListener("click", (event) => {
+
+    imagemnova.src = botao4.src
+    
+});
+
+botao5.addEventListener("click", (event) => {
+
+    imagemnova.src = botao5.src
     
 });
