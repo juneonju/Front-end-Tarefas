20754e686e84b41ef28e938485075ecd8f4ccf33
José Bulhões <josebgojunior@gmail.com>
2024-03-09 18:49:25 -0300

    decimo terceiro commit

---
+++ gerador-senha-teste/teste.js
@@ -0,0 +1,43 @@
+const chamarsenha = document.querySelector("#texto1")
+const ListadeSenhas = document.querySelector("#texto2")
+const botao = document.querySelector("#botao")
+
+let contadorRegular = 0;
+let contadorPrioridade = 0;
+let contar = 0;
+
+    botao.addEventListener("click", (event) => {
+        contar++;
+        let condicao = contar % 3;
+        botao.disabled = true;
+        contadorRegular++;
+        chamarsenha.innerHTML = "R" + contadorRegular;
+        let li = document.createElement("li");
+        li.textContent = "R" + contadorRegular;
+        ListadeSenhas.appendChild(li);
+        ListadeSenhas.insertBefore(li, ListadeSenhas.children[0]);
+        let falarRegular = document.createElement("textarea");
+        falarRegular.textContent = "Regular" + contadorRegular;
+        var voz = new SpeechSynthesisUtterance(falarRegular.value);
+        speechSynthesis.speak(voz);
+        setTimeout(() => {
+        botao.disabled = false;
+        }, 2000);
+        
+        if(condicao == 0){
+                botao.disabled = true;
+                contadorPrioridade++;
+                chamarsenha.innerHTML = "P" + contadorPrioridade;
+                let li = document.createElement("li");
+                li.textContent = "P" + contadorPrioridade;
+                ListadeSenhas.appendChild(li);
+                ListadeSenhas.insertBefore(li, ListadeSenhas.children[0]);
+                let falarPrioridade = document.createElement("textarea");
+                falarPrioridade.textContent = "prioridade" + contadorPrioridade;
+                var voz = new SpeechSynthesisUtterance(falarPrioridade.value);
+                speechSynthesis.speak(voz);
+                setTimeout(() => {
+                botao.disabled = false;
+                }, 2000);        
+        }
+});
