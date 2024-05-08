7740e17a2a60172467f4aa301bb6ff9e00f480a7
José Bulhões <josebgojunior@gmail.com>
2024-05-07 21:21:23 -0300

    hihihihihi

--- site-filme/js/inicio.js
+++ site-filme/js/inicio.js
@@ -1,12 +1,17 @@
-const botao = document.querySelector("#botao")
-const areaImg = document.querySelector("#area-img")
+const areaImg = document.querySelector('#div-poster-titulo');
+const img = document.querySelector('#imgPadrao');
+const titulo = document.querySelector('#titulo-filme')
 
+let listaPoster = [];
+let listaTitulos = [];
+let contador = 0;
+let contador2 = 0;
 
-window.onload =  buscar()
+window.onload =  buscar();
 
 function buscar(){
 
-    let url1 = "https://media.themoviedb.org/t/p/w200"
+    let caminhoIMG = "https://media.themoviedb.org/t/p/w200";
 
     let url = "https://api.themoviedb.org/3/movie/popular?language=en-US&page=1";
 
@@ -16,22 +21,44 @@ function buscar(){
     request.send();
     console.log(url);
 
-    let lista = JSON.parse(request.response)
-    console.log(lista)
+    let lista = JSON.parse(request.response);
+    console.log(lista);
 
     for (let index = 0; index < lista.results.length; index++) {
-        let poster = document.createElement("img")
-        poster.className = "imgPadrao"
-        let divInfo = document.createElement("div")
-        poster.src = url1 + lista.results[index].poster_path
+        let poster = document.createElement("img");
+        poster.src = caminhoIMG + lista.results[index].poster_path;
+        
+        listaPoster.push(poster);
 
-        divInfo.appendChild(poster)
-        areaImg.appendChild(divInfo)
+    }
 
-        botao.addEventListener('click', (event) => {
+    img.src = listaPoster[0].src;
+        setInterval(() => {
+        if (contador >= listaPoster.length) {
+            contador = 0;
+        }
+        img.src = listaPoster[contador].src;
+        contador++;
+    }, 3000);
 
-        });
+
+    for (let index = 0; index < lista.results.length; index++) {
+        let titulos = document.createElement("h3");
+        titulos.className = "titulos";
+        titulos.textContent = lista.results[index].title;
+
+        listaTitulos.push(titulos);
         
     }
+
+    titulo.textContent = listaTitulos[0].textContent
+        setInterval(() => {
+            if (contador2 >= listaTitulos.length) {
+                contador2 = 0;
+            }
+            titulo.textContent = listaTitulos[contador2].textContent;
+            contador2++;
+        }, 3000);
+
 }
   
